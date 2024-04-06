9faf968b8984f8826fc9962a15745ca206fdf7e4
Sebastião Soares da Silva Filho <123883250+seb2301@users.noreply.github.com>
2024-04-06 11:02:04 -0300

    Add files via upload

---
+++ Exercício 14.cpp
@@ -0,0 +1,38 @@
+//14 FUP que solicite dois numero e verifique qual é o maior;
+
+#include  <iostream> 
+
+using namespace std; 
+
+
+main (){
+	system("chcp 65001");
+	
+	float num, num2;
+	
+	cout<<"Digite um número: ";
+	cin>>num;
+	
+	cout<<"Digite outro número: ";
+	cin>>num2;
+	
+	if(num >= num2){
+		cout<<"O Maior número é: "<<num;
+	}
+		
+		else if(num2 >= num){
+		
+		cout<<"O Maior número é: "<<num2;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Exercício 15.cpp
@@ -0,0 +1,33 @@
+// 15 FUP que leia 3 valores a,b,c e verifique se eles formam ou não um triângulo. Caso os valores formem um triângulo, solicite a base e a altura, calcule (base * altura / 2) e escreva a área deste triângulo. Se não formam triângulo escreva os valores lidos.
+
+#include  <iostream> 
+
+using namespace std; 
+
+
+main (){
+	system("chcp 65001");
+	
+	int num, num2, num3;
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
